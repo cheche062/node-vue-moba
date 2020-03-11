1867f2bbd11dd0b09be5f2526d24aff4bfc97c94
hejianbo <yoojunglee@126.com>
2020-03-11 22:08:07 +0800

    英雄页展示ok

--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -9,7 +9,30 @@
       <router-link to="/" tag="div" class="">更多英雄 &gt;</router-link>
     </div>
 
-    <img :src="model.avatar" alt="">
+    <!-- <img :src="model.avatar" alt=""> -->
+
+    <div class="top d-flex jc-end flex-column" :style="{'background-image': `url(${model.banner})`}">
+      <div class="info text-white p-3">
+        <div class="fs-sm">{{model.title}}</div>
+        <h2 class="my-2">{{model.name}}</h2>
+        <div class="fs-sm">{{[...model.categories].map(v=>v.name).join('/')}}</div>
+        <div class="d-flex jc-between pt-2">
+          <div class="scores d-flex ai-center" v-if="model.scores">
+            <span>难度</span>
+            <span class="badge bg-primary">{{model.scores.difficult}}</span>
+            <span>技能</span>
+            <span class="badge bg-blue">{{model.scores.skills}}</span>
+            <span>攻击</span>
+            <span class="badge bg-danger">{{model.scores.attack}}</span>
+            <span>生存</span>
+            <span class="badge bg-info">{{model.scores.survive}}</span>
+          </div>
+          <router-link to="/" tag="span" class="text-gray fs-sm">
+            皮肤: 2 &gt;
+          </router-link>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -41,4 +64,29 @@ export default {
 </script>
 
 <style lang="scss">
+.page-hero{
+  .top{
+    height: 50vw;
+    background-size: 100% auto;
+  }
+
+  .info{
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1));
+    .scores{
+      .badge{
+        margin: 0 0.25rem;
+        display: inline-block;
+        width: 1rem;
+        height: 1rem;
+        line-height: 1rem;
+        text-align: center;
+        border-radius: 50%;
+        font-size: 0.8rem;
+        border: 1px solid rgba(255, 255, 255, 0.6);
+      }
+    }
+  }
+
+}
+
 </style>
