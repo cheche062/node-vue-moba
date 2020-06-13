0f34b4c74ccd38744c015c1c179e53c0d4e5ad3a
hejianbo <yoojunglee@126.com>
2020-06-13 18:12:46 +0800

    删除不需要的图片

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  //指定打包后的输出路径
   outputDir: __dirname + '/../server/admin',
+  // 公共的引用路径（和上边输出路径的admin是一致的）
   publicPath: process.env.NODE_ENV === 'production' ? '/admin/' : '/'
 }
